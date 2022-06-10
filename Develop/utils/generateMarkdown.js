edc66a44dccbbb40a773e9eb1c8bb14f49b6d657
DlonMusk <knight777@hotmail.ca>
2022-06-09 22:23:28 -0400

    fixed title to display in readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -28,7 +28,7 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-  # <${data.title}>
+  # ${data.title}
   ${renderLicenseBadge(data.license)}
   ## Description
   ${data.description}
